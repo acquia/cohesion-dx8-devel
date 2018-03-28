9b9f44a807925f7a831b332974b31c0e914d2aeb
Clive Linsell <clive.linsell@coherence.co.uk>
2018-03-28 09:54:44 +0000

    Make repository public
    composer.json edited online with Bitbucket
    
    composer.json edited online with Bitbucket
    
    Upgrade sass variables controller for new dx8 entity types
    
    Fix Sass variables page
    
    Updated name to Site Studio
    
    Updated module group.
    
    Add required files to make repo public

--- src/Controller/SassVariablesController.php
+++ src/Controller/SassVariablesController.php
@@ -6,32 +6,52 @@ use Drupal\Component\Serialization\Json;
 use Drupal\Core\Controller\ControllerBase;
 use Symfony\Component\HttpFoundation\Request;
 
-class SassVariablesController extends ControllerBase
-{
+class SassVariablesController extends ControllerBase {
 
-  function view(Request $request)
-  {
+  function view(Request $request) {
     return [
       '#theme' => 'sass_variables',
-      '#colours' => $this->colourVars()
+      '#colours' => $this->colourVars(),
+      '#cache' => [
+        'max-age' => 0,
+      ],
     ];
   }
 
-  protected function colourVars()
-  {
-    $palette = $this->entityTypeManager()
-      ->getStorage('cohesion_website_settings')
-      ->load('color_palette');
-
-    $data = Json::decode($palette->get('json_values'));
-
+  protected function colourVars() {
     $colours = [];
-    foreach ($data['colors'] as $item) {
-      $colours[] = $item['variable'] . ': ' . $item['value']['value']['rgba'] . ';';
+    $module = system_get_info('module', 'cohesion');
+
+    if (version_compare('8.x-3.11', $module['version']) === -1) {
+      // New (>=5.0)
+      $colour_entities = $this->entityTypeManager()
+        ->getStorage('cohesion_color')
+        ->loadMultiple();
+
+      foreach ($colour_entities as $colour) {
+        $item = Json::decode($colour->get('json_values'));
+        $colours[] = $this->formatColourLine($item);
+      }
+    }
+    else {
+      // Old (<=3.11)
+      $palette = $this->entityTypeManager()
+        ->getStorage('cohesion_website_settings')
+        ->load('color_palette');
+
+      $data = Json::decode($palette->get('json_values'));
+
+      $colours = [];
+      foreach ($data['colors'] as $item) {
+        $colours[] = $this->formatColourLine($item);
+      }
     }
 
     return $colours;
+  }
 
+  protected function formatColourLine($item) {
+    return $item['variable'] . ': ' . $item['value']['value']['rgba'] . ';';
   }
 
 }
