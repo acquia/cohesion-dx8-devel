8f02441bfa9f1eb8d1cc2f68eb0d728813ac3565
Clive Linsell <clive.linsell@gmail.com>
2018-12-10 21:24:44 +0000

    Fix Sass variables page

--- src/Controller/SassVariablesController.php
+++ src/Controller/SassVariablesController.php
@@ -2,34 +2,35 @@
 
 namespace Drupal\cohesion_devel\Controller;
 
-use Drupal\Component\Plugin\Exception\PluginNotFoundException;
 use Drupal\Component\Serialization\Json;
 use Drupal\Core\Controller\ControllerBase;
 use Symfony\Component\HttpFoundation\Request;
 
-class SassVariablesController extends ControllerBase
-{
+class SassVariablesController extends ControllerBase {
 
-  function view(Request $request)
-  {
+  function view(Request $request) {
     return [
       '#theme' => 'sass_variables',
-      '#colours' => $this->colourVars()
+      '#colours' => $this->colourVars(),
+      '#cache' => [
+        'max-age' => 0,
+      ],
     ];
   }
 
-  protected function colourVars()
-  {
+  protected function colourVars() {
+    $colours = [];
     $module = system_get_info('module', 'cohesion');
+
     if (version_compare('8.x-3.11', $module['version']) === -1) {
       // New (>=5.0)
-      $colour_entities = $this->entityTypeManager
+      $colour_entities = $this->entityTypeManager()
         ->getStorage('cohesion_color')
         ->loadMultiple();
 
       foreach ($colour_entities as $colour) {
-        $data = Json::decode($colour->get('json_values'));
-        $colours[] = $data['variable'] . ': ' . $data['value']['value']['rgba'] . ';';
+        $item = Json::decode($colour->get('json_values'));
+        $colours[] = $this->formatColourLine($item);
       }
     }
     else {
@@ -42,12 +43,15 @@ class SassVariablesController extends ControllerBase
 
       $colours = [];
       foreach ($data['colors'] as $item) {
-        $colours[] = $item['variable'] . ': ' . $item['value']['value']['rgba'] . ';';
+        $colours[] = $this->formatColourLine($item);
       }
     }
 
     return $colours;
+  }
 
+  protected function formatColourLine($item) {
+    return $item['variable'] . ': ' . $item['value']['value']['rgba'] . ';';
   }
 
 }
